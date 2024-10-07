722debc39a7f0b97a01b101448af242ddfae57fe
Gil CasRam <gcasram7@gmail.com>
2024-10-07 14:04:06 -0600

    Initial Commit

---
+++ MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,20 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Gil casimiro on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
